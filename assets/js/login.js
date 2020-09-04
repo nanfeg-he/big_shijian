bdd315a24ef0a502995bc7b0b8cdca741d5dac31
cuihehe <1912138979@qq.com>
2020-09-04 10:21:07 +0800

    添加了全局设置的url的js

--- assets/js/login.js
+++ assets/js/login.js
@@ -43,7 +43,7 @@ $(function () {
             password: $('#form_reg [name=password]').val()
         }
         // $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){console.log(res)})
-        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message)
             }
@@ -56,7 +56,7 @@ $(function () {
         e.preventDefault()
         //发起ajax请求
         $.ajax({
-            url: 'http://ajax.frontend.itheima.net/api/login',
+            url: '/api/login',
             type: "POST",
             data: $(this).serialize(),
             success: function (res) {
